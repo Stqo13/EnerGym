90ac2c6659bbe59d154f6f11d7bb34462bd5f45e
Stqo <stefandomitrov136@gmail.com>
2024-11-12 21:40:33 +0200

    Added workout service

---
+++ EnerGym.Services.Data/Implementations/WorkoutService.cs
@@ -0,0 +1,28 @@
+﻿using EnerGym.Data.Models;
+using EnerGym.Services.Data.Interfaces;
+
+namespace EnerGym.Services.Data.Implementations
+{
+    public class WorkoutService : IWorkoutSevice
+    {
+        public Task CreateAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task EditAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<WorkoutPlan>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
