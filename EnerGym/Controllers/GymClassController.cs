3cdd8f5a4730c199b5464a8630989e1023731254
Stqo <stefandomitrov136@gmail.com>
2024-12-08 21:21:59 +0200

    deleted unused files

--- EnerGym/Controllers/GymClassController.cs
+++ EnerGym/Controllers/GymClassController.cs
@@ -2,12 +2,23 @@
 
 namespace EnerGym.Controllers
 {
-    public class GymClassController : Controller
+    public class GymClassController (
+        ILogger<GymClassController> logger
+        )
+        : Controller
     {
         [HttpGet]
         public async Task<IActionResult> Index()
         {
-            return View();
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"An error occured while trying to fetch all gym classes. {ex.Message}");
+                return RedirectToAction("Error", "Home");
+            }
         }
     }
 }
